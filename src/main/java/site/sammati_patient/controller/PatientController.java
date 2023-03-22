3379367ca2547f7d23c99736af85a5adf9a8365e
gautamrizwani <gautamrizwani@gmail.com>
2023-03-22 10:48:54 +0530

    Added sendOTP() API

--- src/main/java/site/sammati_patient/controller/PatientController.java
+++ src/main/java/site/sammati_patient/controller/PatientController.java
@@ -9,6 +9,11 @@ import site.sammati_patient.dto.PatientLoginDto;
 import site.sammati_patient.entity.Patient;
 import site.sammati_patient.service.PatientService;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
 @RestController
@@ -46,5 +51,39 @@ public class PatientController {
         return patient;
     }
 
+    @PostMapping("/send_otp")
+    public Integer sendOTP(@RequestParam("phoneNumber") String phno, @RequestParam("message") String message) {
+        //Third party API call
+        int code=-1;
+        try
+        {
+            String apiKey="Tf0l5vJFUNs3EqRDwmVpjkIHLbYXSBtxGicuhyPQdW9nr2e178brnxh0MUt2DmlTGoPKJLEvaAYN9SW6";
+            String sendId="FSTSMS";
+            message= URLEncoder.encode(message, "UTF-8");
+            String language="english";
+            String route="p";
+            String myUrl="https://www.fast2sms.com/dev/bulk?authorization="+apiKey+"&sender_id="+sendId+"&message="+message+"&language="+language+"&route="+route+"&numbers="+phno;
 
+            URL url=new URL(myUrl);
+            HttpsURLConnection con=(HttpsURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            con.setRequestProperty("cache-control", "no-cache");
+            code=con.getResponseCode();
+            StringBuffer response=new StringBuffer();
+            BufferedReader br=new BufferedReader(new InputStreamReader(con.getInputStream()));
+            while(true)
+            {
+                String line=br.readLine();
+                if(line==null)
+                {
+                    break;
+                }
+                response.append(line);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return code;
+    }
 }
