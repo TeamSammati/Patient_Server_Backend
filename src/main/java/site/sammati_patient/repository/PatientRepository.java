e4b2a95163e8a97ac6123bed2e6da5df76866488
Hansal21 <hansaldhandha123@gmail.com>
2023-04-03 19:45:09 +0530

    Added qr code and patient's photo api

--- src/main/java/site/sammati_patient/repository/PatientRepository.java
+++ src/main/java/site/sammati_patient/repository/PatientRepository.java
@@ -1,5 +1,7 @@
 package site.sammati_patient.repository;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import site.sammati_patient.entity.Patient;
@@ -17,4 +19,9 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
 
     @Query("select p.patientId from Patient p where p.patientId=?1")
     Integer patientExist(Integer patientId);
+
+    @Modifying
+    @Transactional
+    @Query("update Patient set qrCode=?1 where patientId=?2")
+    Integer addQR(String databytes, Integer patientId);
 }
